060c2e25f87b69d6c126474f32bec378dade5573
neilcore <neilcore22@gmail.com>
2025-07-30 01:47:04 +0800

    write test for CountryService

--- backend/src/test/java/core/hubby/backend/BackendApplicationTests.java
+++ backend/src/test/java/core/hubby/backend/BackendApplicationTests.java
@@ -1,22 +1,55 @@
 package core.hubby.backend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.PostgreSQLContainer;
 
+import core.hubby.backend.business.entities.embedded.DefaultCurrency;
+import core.hubby.backend.core.service.CountryService;
+
+/**
+ * It tells Spring to include the beans defined in
+ * TestcontainersConfiguration (specifically your PostgreSQLContainer bean)
+ * when building the application context.
+ */
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class BackendApplicationTests {
+	
+	@Autowired
+	CountryService countryService;
 	
 	@Test
 	void contextLoads() {
 	}
 	
+	/**
+	 * Test some third party services: CountryService
+	 */
 	@Test
-	void doNothing() {
-		System.out.println("Hello world");
+	void validateCountry() {
+		boolean response = countryService
+				.validateCountry("QX");
+		
+		assertThat(response)
+		.isNotEqualTo(true)
+		.isEqualTo(false);
+	}
+	
+	@Test
+	void returnCurrency() {
+		DefaultCurrency response = 
+				countryService.returnCurrency("PH");
+		
+		assertThat(response.getName())
+		.contains("Philippine", "peso");
+		
+		assertThat(response.getSymbol())
+		.doesNotContain("$")
+		.isEqualTo("₱");
 	}
 
 }
