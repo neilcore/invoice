8b1d9d98df16f118fd1a811ab5dcc0b15bbb9232
neilcore <neilcore22@gmail.com>
2025-07-30 01:46:47 +0800

    add @toSAtring lombok annotation

--- backend/src/main/java/core/hubby/backend/business/entities/embedded/DefaultCurrency.java
+++ backend/src/main/java/core/hubby/backend/business/entities/embedded/DefaultCurrency.java
@@ -8,9 +8,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Getter
 @Setter
 @Builder
